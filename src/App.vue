c9909205dd9b79a307f4e6e4a3a96721d36cd2ab
zealer-young <nightwish.ethan@gmail.com>
2020-03-23 02:50:18 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
-
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   },
   data() {
     return {
-      age:30
+      
     }
   },
   mounted() {
-    let url="https://www.easy-mock.com/mock/5e772b5b4ecfa92432bfd5da/example_copy";
-    axios.get(url).then(() => {
-
-    })
+    
   }
 }
 
