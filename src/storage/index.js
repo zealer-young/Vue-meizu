1e71b569dd4893956012b5486254dc0c00004d71
zealer-young <nightwish.ethan@gmail.com>
2020-03-24 06:42:52 +0800

    环境设置

--- src/storage/index.js
+++ src/storage/index.js
@@ -32,6 +32,7 @@
      clear(key,module_name) {
          let val = this.getStorage();
          if(module_name) {
+             if(!val[module_name]) return;
              delete val[module_name][key];
          }else {
              delete val[key];
