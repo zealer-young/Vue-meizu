f2b946b60f92efb0f1f3f47f1e541a7e7e75a7a7
zealer-young <nightwish.ethan@gmail.com>
2020-03-25 02:17:03 +0800

    4-12 mock设置

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,12 @@ export default {
     //storage.setItem('user',{a:1})
 
     //本地加载请求静态json文件的形式
-    this.axios.get('/mock/user/login.json').then((res) => {
+    // this.axios.get('/mock/user/login.json').then((res) => {
+    //     this.res = res;
+    // });
+
+     //通过easy-mock平台实现数据mock
+    this.axios.get('/user/login').then((res) => {
         this.res = res;
     });
   }
