94f1f98ae92f9a470c925e76f15253a731cbf2cd
zealer-young <nightwish.ethan@gmail.com>
2020-03-27 08:20:27 +0800

    NavFooter 组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,8 +1,60 @@
 <template>
-    <div></div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="../../public/imgs/logo-footer.png" alt />
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="https://www.baidu.com" target="_blank">小米商城</a>
+      <span>|</span>
+      <a href="https://www.baidu.com" target="_blank">小米手机</a>
+      <span>|</span>
+      <a href="https://www.baidu.com" target="_blank">小米电视</a>
+      <span>|</span>
+      <a href="https://www.baidu.com" target="_blank">小米有品</a>
+    </div>
+    <div class="copyright">
+      Copyright ©2019
+      <span class="domain">mi.futurefe.com</span> All Rights Reserved.
+    </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'nav-footer',
-}
+  name: "nav-footer"
+};
 </script>
+<style lang="scss" scoped>
+.footer {
+  height: 234px;
+  border-top: 4px solid #ff6600;
+  background-color: #333333;
+  color: #999999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo {
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link {
+    a {
+      color: #999999;
+      display: inline-block;
+    }
+    span {
+      margin: 0 10px;
+    }
+    margin-bottom: 13px;
+  }
+  .copyright {
+    .domain {
+      color: #ff6600;
+    }
+  }
+}
+</style>
