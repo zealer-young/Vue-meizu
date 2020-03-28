4baabeb6ba7a086b6311a96162d831faaf91514b
zealer-young <nightwish.ethan@gmail.com>
2020-03-28 09:33:23 +0800

    手机商品静态页面实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -64,7 +64,33 @@
           <img src="../../public/imgs/banner-1.png" alt />
         </a>
       </div>
-      <div class="product-box"></div>
+    </div>
+    <div class="product-box">
+      <div class="container">
+        <h2>手机</h2>
+        <div class="wrapper">
+          <div class="banner-left">
+            <a href="/#/product/35">
+              <img src="../../public/imgs/mix-alpha.jpg" alt />
+            </a>
+          </div>
+          <div class="list-box">
+            <div class="list" v-for="(arr,i) in phoneList" v-bind:key="i">
+              <div class="item" v-for="(item,j) in arr" v-bind:key="j">
+                <span>新品</span>
+                <div class="item-img">
+                  <img src="../../public/imgs/detail/phone-1.jpg" alt />
+                </div>
+                <div class="item-info">
+                  <h3>小米9</h3>
+                  <p>小龙855，索尼4800万超广角微距</p>
+                  <p class="price">2999元</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <service-bar></service-bar>
   </div>
@@ -170,6 +196,10 @@ export default {
           id: 47,
           img: require("../../public/imgs/ads/ads-4.jpg")
         }
+      ],
+      phoneList: [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
       ]
     };
   }
@@ -268,6 +298,79 @@ export default {
   }
   .banner {
     margin-bottom: 50px;
+  }
+  .product-box {
+    background-color: $colorJ;
+    padding: 30px 0 50px;
+    h2 {
+      font-size: $fontF;
+      height: 21px;
+      line-height: 21px;
+      color: $colorB;
+      margin-bottom: 20px;
+    }
+    .wrapper {
+      display: flex;
+      .banner-left {
+        margin-right: 16px;
+        img {
+          width: 224px;
+          height: 619px;
+        }
+      }
+      .list-box {
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+            span {
+            }
+            .item-img {
+              img {
+                height: 195px;
+              }
+            }
+            .item-info {
+              h3 {
+                font-size: $fontJ;
+                color: $colorB;
+                line-height: $fontJ;
+                font-weight: bold;
+              }
+              p {
+                color: $colorD;
+                line-height: 13px;
+                margin: 6px auto 13px;
+              }
+              .price {
+                color: #f20a0a;
+                font-size: $fontJ;
+                font-weight: bold;
+                cursor: pointer;
+                &:after {
+                  content: " ";
+                  margin-left: 5px;
+                  vertical-align: middle;
+                  @include bgImg(
+                    22px,
+                    22px,
+                    "../../public/imgs/icon-cart-hover.png"
+                  );
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
