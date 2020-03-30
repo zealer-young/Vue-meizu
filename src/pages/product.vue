bd7377b8068c3ae77f2f0e815499b925372b7dce
zealer-young <nightwish.ethan@gmail.com>
2020-03-30 22:50:45 +0800

    Slide动画实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -58,11 +58,11 @@
           后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！
           <br />更能AI 精准分析视频内容，15个场景智能匹配背景音效。
         </p>
-        <div class="video-bg"></div>
+        <div class="video-bg" @click="showSlide=true"></div>
         <div class="video-box">
-          <div class="overlay"></div>
-          <div class="video">
-            <span class="icon-close"></span>
+          <div class="overlay" v-if="showSlide"></div>
+          <div class="video" v-bind:class="{'slide':showSlide}">
+            <span class="icon-close" @click="showSlide=false"></span>
             <!-- video标签 muted属性设置静音输出 autoplay自动播放-->
             <video src="/imgs/product/video.mp4" muted autoplay controls="controls"></video>
           </div>
@@ -83,7 +83,7 @@ export default {
   },
   data() {
     return {
-      showSlide: "", //控制动画效果
+      showSlide: false, //控制动画效果
       product: {}, //商品信息
       swiperOption: {
         autoplay: true,
@@ -201,7 +201,6 @@ export default {
         cursor: pointer;
       }
       .video-box{
-        z-index: 100;
         .overlay{
           @include position(fixed);
           background-color: #333333;
@@ -210,12 +209,18 @@ export default {
         }
         .video{
           position:fixed;
-          top:50%;
+          top:-50%;
           left: 50%;
           transform: translate(-50%,-50%);
           z-index: 10;
           width: 1000px;
           height:536px;
+          opacity: 0;
+          transition: all .6s;
+          &.slide{
+            top:50%;
+            opacity: 1;
+          }
           .icon-close{
             position:absolute;
             top:20px;
