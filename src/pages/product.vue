5bacc37abc0cff38928b2a0f07ea1323b75ad7b5
zealer-young <nightwish.ethan@gmail.com>
2020-03-30 23:31:47 +0800

    Slide-animation动画实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -58,11 +58,11 @@
           后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！
           <br />更能AI 精准分析视频内容，15个场景智能匹配背景音效。
         </p>
-        <div class="video-bg" @click="showSlide=true"></div>
+        <div class="video-bg" @click="showSlide='slideDown'"></div>
         <div class="video-box">
-          <div class="overlay" v-if="showSlide"></div>
-          <div class="video" v-bind:class="{'slide':showSlide}">
-            <span class="icon-close" @click="showSlide=false"></span>
+          <div class="overlay" v-if="showSlide=='slideDown'"></div>
+          <div class="video" v-bind:class="showSlide">
+            <span class="icon-close" @click="showSlide='slideUp'"></span>
             <!-- video标签 muted属性设置静音输出 autoplay自动播放-->
             <video src="/imgs/product/video.mp4" muted autoplay controls="controls"></video>
           </div>
@@ -207,6 +207,26 @@ export default {
           opacity: .4;
           z-index: 10;
         }
+        //animation 动画的定义
+        @keyframes slideDown {
+          from{
+            top:-50%;
+            opacity: 0;
+          }to{
+            top:50%;
+            opacity: 1;   
+          }
+        }
+         @keyframes slideUp {
+          from{
+            
+            top:50%;
+            opacity: 1; 
+          }to{
+            top:-50%;
+            opacity: 0;  
+          }
+        }
         .video{
           position:fixed;
           top:-50%;
@@ -215,11 +235,14 @@ export default {
           z-index: 10;
           width: 1000px;
           height:536px;
-          opacity: 0;
-          transition: all .6s;
-          &.slide{
+          opacity: 1;
+          &.slideDown{
+            animation: slideDown .6s linear;
             top:50%;
-            opacity: 1;
+          }
+          &.slideUp{
+            animation: slideUp .6s linear;
+            
           }
           .icon-close{
             position:absolute;
