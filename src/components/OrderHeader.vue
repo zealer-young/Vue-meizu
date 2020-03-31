dc08391684adb6e16be7791546ad67af369b94c6
zealer-young <nightwish.ethan@gmail.com>
2020-04-01 02:30:36 +0800

    补充

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -30,6 +30,7 @@ export default {
 <style lang="scss">
     .order-header{
         padding: 30px 0;
+        border-bottom: 2px solid #ff6600;
         .header-logo{
             float: left;
         }
@@ -39,15 +40,17 @@ export default {
             line-height: 55px;
         }
         .title{
-            font-size: 28px;
             float: left;
-            color: #333333;
             margin-left: 54px;
+            h2{
+              font-size: 28px;
+              color: #333333;
+            }
             span{
                 font-size: 14px;
                 margin-left: 17px;
                 color:#999999;
-                font-weight: bold;
+                font-weight: 200;
             }
         }
         .username{
