e99e0f202328a08525bf1d31eeca79f7eb4571c8
zealer-young <nightwish.ethan@gmail.com>
2020-04-02 15:49:21 +0800

    新增地址表单静态实现

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -105,7 +105,49 @@
       </div>
     </div>
     <modal
-    title="确认删除"
+    title="新增确认"
+    btnType="1"
+    :showModal="showEditModal"
+    @cancel="showEditModal=false"
+    @submit="submitAddress"
+    >
+    <template v-slot:body>
+      <div class="edit-wrap">
+        <div class="item">
+         <input type="text" class="input" placeholder="姓名"> 
+         <input type="text" class="input" placeholder="手机号"> 
+        </div>
+        <div class="item">
+          <select name="province">
+            <option value="北京">北京</option>
+            <option value="天津">天津</option>
+            <option value="河北">河北</option>
+          </select>
+          <select name="city">
+            <option value="北京">北京</option>
+            <option value="天津">天津</option>
+            <option value="河北">石家庄</option>
+          </select>
+          <select name="district">
+              <option value="北京">昌平区</option>
+              <option value="天津">海淀区</option>
+              <option value="河北">东城区</option>
+              <option value="天津">西城区</option>
+              <option value="河北">顺义区</option>
+              <option value="天津">房山区</option>
+          </select>
+        </div>
+        <div class="item">
+          <textarea name="street"></textarea>
+        </div>
+        <div class="item">
+          <input type="text" class="input" placeholder="邮编"> 
+        </div>
+      </div>
+    </template>
+    </modal>
+    <modal
+    title="删除确认"
     btnType="1"
     :showModal="showDelModal"
     @cancel="showDelModal=false"
@@ -130,6 +172,7 @@ export default{
       checkedItem:{},//选中的商品对象
       userAction:'',//用户行为  0：新增  1：编辑  2：删除
       showDelModal:false,//是否显示删除弹框
+      showEditModal:true,//是否显示新增或者编辑弹框
     }
   },
   components:{
@@ -337,6 +380,36 @@ export default{
         .btn-group{
           margin-top: 37px;
           text-align: right;
+        }
+      }
+    }
+    .edit-wrap{
+      font-size: 14px;
+      .item{
+        margin-bottom: 15px;
+        input{
+          display: inline-block;
+          width: 283px;
+          height: 40px;
+          line-height: 40px;
+          padding-left: 15px;
+          border: 1px solid #e5e5e5;
+          &+.input{
+            margin-left: 14px;
+          }
+        }
+        select{
+          height: 40px;
+          line-height: 40px;
+          border: 1px solid #e5e5e5;
+          margin-right:15px;
+        }
+        textarea{
+          height: 62px;
+          width: 100%;
+          padding: 13px 15px;
+          box-sizing: border-box;
+          border: 1px solid #e5e5e5;
         }
       }
     }
