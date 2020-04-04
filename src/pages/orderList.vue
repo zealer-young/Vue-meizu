4f678d379c03e663b61d87622f8e8635dc358633
zealer-young <nightwish.ethan@gmail.com>
2020-04-04 11:40:25 +0800

    订单列表数据渲染

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -1,11 +1,56 @@
 <template>
-    <div>
-      <order-header title="订单列表">
+    <div class="order-list">
+    <order-header title="订单列表">
       <template v-slot:tip>
-        <span>请谨防钓鱼链接或诈骗电话，了解更多</span>
+        <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
       </template>
     </order-header>
+    <div class="wrapper">
+      <div class="container">
+        <div class="order-box">
+
+          <div class="order" v-for="(order,index) in list" :key="index">
+            <div class="order-title">
+              <div class="item-info fl">
+                {{order.createTime}}
+                <span>|</span>
+                {{order.receiverName}}
+                <span>|</span>
+                订单号：{{order.orderNo}}
+                <span>|</span>
+                {{order.paymentTypeDesc}}
+              </div>
+              <div class="item-money fr">
+                <span>应付金额：{{order.payment}}</span>
+                <span class="money"></span>
+                <span>元</span>
+              </div>
+            </div>
+            <div class="order-content clearfix">
+              <div class="good-box fl">
+                <div class="good-list" v-for="(item,i) in order.orderItemVoList" :key="i">
+                  <div class="good-img">
+                    <img v-lazy="item.productImage">
+                  </div>
+                  <div class="good-name">
+                    <div class="p-name">{{item.productName}}</div>
+                    <div class="p-money">{{item.totalPrice + 'X' + item.quantity}}元</div>
+                  </div>
+                </div>
+              </div>
+              <!-- status:订单状态 0-已取消，10-未付款，20-已付款，40-已发货，50-交易成功，60-交易关闭 -->
+              <div class="good-state fr" v-if="order.status == 20">
+                <a href="javascript:;">{{order.statusDesc}}</a>
+              </div>
+              <div class="good-state fr" v-else>
+                <a href="javascript:;" @click="goPay(order.orderNo)">{{order.statusDesc}}</a>
+              </div>
+            </div>
+          </div>          
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import OrderHeader from './../components/OrderHeader'
@@ -13,6 +58,122 @@ export default {
     name: 'order-list',
     components:{
       OrderHeader,
+    },
+    data(){
+      return{
+        list:[],
+      }
+    },
+    mounted(){
+      this.getOrderList();
+    },
+    methods:{
+      getOrderList(){
+        this.axios.get('/orders').then((res)=>{
+        this.list = res.list;
+      })
+      },
+      //去支付
+      goPay(orderNo){
+        // 三种路由跳转方式
+        // 第一种 this.$router.push('/order/pay');
+        //第二种 
+        /*this.$router.push({
+          name:'order-pay',//name这里指路由的名称
+          query:{
+            orderNo
+          }
+        })
+        */
+       //第三种
+       this.$router.push({
+          path:'/order/pay',//path是路径
+          query:{
+            orderNo
+          }
+        })
+        //第二种和第三种的区别是：第二种使用的是路由的名称跳转；第三种使用的是路径跳转
+       // query传参和params传参的区别是：query传参可以将参数添加到地址栏里面去
+      }
     }
 }
 </script>
+<style lang="scss">
+  @import './../assets/scss/config.scss';
+  @import './../assets/scss/mixin.scss';
+  .order-list{
+    .wrapper{
+      background-color:$colorJ;
+      padding-top:33px;
+      padding-bottom:110px;
+      .order-box{
+        .order{
+          @include border();
+          background-color:$colorG;
+          margin-bottom:31px;
+          &:last-child{
+            margin-bottom:0;
+          }
+          .order-title{
+            @include height(74px);
+            background-color:$colorK;
+            padding:0 43px;
+            font-size:16px;
+            color:$colorC;
+            .item-info{
+              span{
+                margin:0 9px;
+              }
+            }
+            .money{
+              font-size:26px;
+              color:$colorB;
+            }
+          }
+          .order-content{
+            padding:0 43px;
+            .good-box{
+              width:88%;
+              .good-list{
+                display: flex;
+                align-items: center;
+                height:145px;
+                .good-img{
+                  width:112px;
+                  img{
+                    width:100%;
+                  }
+                }
+                .good-name{
+                  font-size:20px;
+                  color:$colorB;
+                }
+              }
+            }
+            .good-state{
+              @include height(145px);
+              font-size: 20px;
+              color:$colorA;
+              a{
+                color:$colorA;
+              }
+            }
+          }
+        }
+        .pagination{
+          text-align:right;
+        }
+        .el-pagination.is-background .el-pager li:not(.disabled).active{
+          background-color: #FF6600;
+        }
+        .el-button--primary{
+          background-color: #FF6600;
+          border-color: #FF6600;
+        }
+        .load-more,.scroll-more{
+          text-align:center;
+        }
+      }
+    }
+  }
+</style>
