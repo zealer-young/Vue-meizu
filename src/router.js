a57e5ba9568808eb0a8a00f8630ab6aa20317670
zealer-young <nightwish.ethan@gmail.com>
2020-04-06 05:46:11 +0800

    路由按需加载方案:import插件

--- src/router.js
+++ src/router.js
@@ -4,15 +4,17 @@ import Home from './pages/home'
 import Index from './pages/index'
 // import Product from './pages/product'
 // import Detail from './pages/detail'
-import Cart from './pages/cart'
+// import Cart from './pages/cart'
 // import Login from './pages/login'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
-import OrderPay from './pages/orderPay'
-import AliPay from './pages/alipay'
+// import Order from './pages/order'
+// import OrderConfirm from './pages/orderConfirm'
+// import OrderList from './pages/orderList'
+// import OrderPay from './pages/orderPay'
+// import AliPay from './pages/alipay'
 Vue.use(Router);
 
+//路由按需加载方案：1.可以安装@babel/plugin-syntax-dynamic-import 插件   2.利用import语法
+  
 export default new Router({
     routes: [
         {
@@ -29,49 +31,56 @@ export default new Router({
                 {
                     path: '/product/:id',
                     name: 'product',
-                    component: resolve => require(['./pages/index.vue'],resolve),
+                    /*利用Promise语法实现路由按需加载方案，不用安装插件
+                     component: resolve => require(['./pages/product.vue'],resolve),
+                     */
+                
+                    //利用import语法实现路由按需加载方案,需要安装babel/plugin-syntax-dynamic-import插件
+                    component: () => import('./pages/product.vue')
                 },
                 {
                     path: '/detail/:id',
                     name: 'detail',
-                    component: resolve => require(['./pages/detail.vue'],resolve),
+                    component: () => import('./pages/detail.vue')
                 }
             ]
         },
         {
             path: '/cart',
             name: 'cart',
-            component: Cart,
+            component: () => import('./pages/cart.vue')
         },
         {
             path: '/login',
             name: 'login',
-            component: resolve => require(['./pages/login.vue'],resolve),
+            //利用@babel/plugin-syntax-dynamic-import插件实现路由按需加载方案的语法
+            component: () => import('./pages/login.vue'),
         },
         {
             path: '/order',
             name: 'order',
-            component: Order,  
+            component: () => import('./pages/order.vue'), 
             children: [
                 {
                     path: 'list',
                     name: 'order-list',
-                    component: OrderList,
+                    component: () => import('./pages/orderList.vue'), 
                 },
                 {
                     path: 'confirm',
                     name: 'order-confirm',
-                    component: OrderConfirm,
+                    component: () => import('./pages/orderConfirm.vue')
                 },
                 {
                     path: 'pay',
                     name: 'order-pay',
-                    component: OrderPay,
+                    component: () => import('./pages/orderPay.vue')
                 },
                 {
                     path: 'alipay',
                     name: 'alipay',
-                    component: AliPay,
+                    //利用Promise语法实现路由按需加载方案，不用安装插件
+                    component: () => import('./pages/alipay.vue')
                 }
             ]
         }
