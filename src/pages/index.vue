9871d6f8e73cd5a6a2d47293461ab4e1404d2e07
zealer-young <nightwish.ethan@gmail.com>
2020-05-17 00:27:17 +0800

    add http://mall-pre.springboot.cn

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -220,7 +220,7 @@ export default {
     init() {
       //为给定 ID 的 user 创建请求,params用来传递参数
       this.axios
-        .get("/products", {//使用get请求
+        .get("http://mall-pre.springboot.cn/products", {//使用get请求
           params: {
             categoryId: 100012,
             pageSize: 14
@@ -234,7 +234,7 @@ export default {
         });
     },
     addCart(id){
-      this.axios.post('/carts',{//使用post请求
+      this.axios.post('http://mall-pre.springboot.cn/carts',{//使用post请求
         productId:id,
         selected:true
       }).then((res)=>{
