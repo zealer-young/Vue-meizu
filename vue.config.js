dbde6556f93aca4ac464584e0edd30edd6471f01
zealer-young <nightwish.ethan@gmail.com>
2020-05-17 00:16:19 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -8,17 +8,17 @@ module.exports = {
     port:8080,
     // 告诉dev-server在服务器启动后打开浏览器，将其设置true为打开默认浏览器
     open: true,
-    proxy:{
-      '/api':{
-        target:'http://mall-pre.springboot.cn',
-        changeOrigin:true,
-        pathRewrite:{
-          '/api':''
-        }
-      }
-    }
+    // proxy:{
+    //   '/api':{
+    //     target:'http://mall-pre.springboot.cn',
+    //     changeOrigin:true,
+    //     pathRewrite:{
+    //       '/api':''
+    //     }
+    //   }
+    // }
   },
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? './' : '/',
   // outputDir:'dest',
   // indexPath:'index2.html',
   // lintOnSave:false,
