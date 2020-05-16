9404353a443e88487eb801e64bd9826d2f1f5344
zealer-young <nightwish.ethan@gmail.com>
2020-05-17 00:42:06 +0800

    vue config use proxy

--- vue.config.js
+++ vue.config.js
@@ -8,15 +8,15 @@ module.exports = {
     port:8080,
     // 告诉dev-server在服务器启动后打开浏览器，将其设置true为打开默认浏览器
     open: true,
-    // proxy:{
-    //   '/api':{
-    //     target:'http://mall-pre.springboot.cn',
-    //     changeOrigin:true,
-    //     pathRewrite:{
-    //       '/api':''
-    //     }
-    //   }
-    // }
+    proxy:{
+      '/api':{
+        target:'http://mall-pre.springboot.cn',
+        changeOrigin:true,
+        pathRewrite:{
+          '/api':''
+        }
+      }
+    }
   },
   publicPath: process.env.NODE_ENV === "production" ? './' : '/',
   // outputDir:'dest',
