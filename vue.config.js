1289662aa5848e46db11d0056b3035d3b7ebfd91
zealer-young <nightwish.ethan@gmail.com>
2020-05-17 17:17:57 +0800

    no

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,8 @@ const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 module.exports = {
   lintOnSave: false,
   //删除预加载设置项
-  // chainWebpack: config => {
-  //   config.plugins.delete("prefetch");
-  // },
   chainWebpack: config => {
+    config.plugins.delete("prefetch");
     // 修复HMR
     config.resolve.symlinks(true);
   },
@@ -17,18 +15,18 @@ module.exports = {
     // port:8080,
     // 告诉dev-server在服务器启动后打开浏览器，将其设置true为打开默认浏览器
     open: true,
-    proxy: {
-      "/api": {
-        target: 
-        "http://mall-pre.springboot.cn",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        pathRewrite: {
-          "^/api": "/"
-        }
-      }
-    }
+    // proxy: {
+    //   "/api": {
+    //     target: 
+    //     "http://mall-pre.springboot.cn",
+    //     changeOrigin: true,
+    //     secure: false,
+    //     ws: true,
+    //     pathRewrite: {
+    //       "^/api": "/"
+    //     }
+    //   }
+    // }
   },
   runtimeCompiler: true,
   publicPath: IS_PROD ? "./" : "/",
