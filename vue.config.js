c36f84c7050526bbd320ed8953c28c1aad536bd8
zealer-young <nightwish.ethan@gmail.com>
2020-05-17 15:51:52 +0800

    add public path

--- vue.config.js
+++ vue.config.js
@@ -5,26 +5,26 @@ const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 module.exports = {
   lintOnSave: false,
   //删除预加载设置项
-  chainWebpack: (config) => {
-    config.plugins.delete('prefetch');
+  chainWebpack: config => {
+    config.plugins.delete("prefetch");
   },
-  devServer:{
+  devServer: {
     // host:'localhost',
     // port:8080,
     // 告诉dev-server在服务器启动后打开浏览器，将其设置true为打开默认浏览器
     open: true,
-    proxy:{
-      "/api":{
-        target: 'http://mall-pre.springboot.cn',
-        changeOrigin:true,
+    proxy: {
+      "/api": {
+        target: "http://mall-pre.springboot.cn",
+        changeOrigin: true,
         secure: false,
-        pathRewrite:{
+        pathRewrite: {
           "^/api": ""
         }
       }
     }
   },
-  publicPath: IS_PROD ? './' : '/',
+  publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : "/",
   productionSourceMap: false,
   configureWebpack: config => {
     config.optimization && (config.optimization.splitChunks.minSize = 10000);
@@ -33,9 +33,11 @@ module.exports = {
       config.plugins.push(
         // Webpack完成捆绑过程后要执行的命令：删除dist目录下ignore目录，prod-md5-zip目录
         new FileManagerPlugin({
-          onEnd: [{
-            delete: [`./dist/ignore`, "./prod-md5-zip"]
-          }]
+          onEnd: [
+            {
+              delete: [`./dist/ignore`, "./prod-md5-zip"]
+            }
+          ]
         })
       );
     }
@@ -45,4 +47,4 @@ module.exports = {
       maxAssetSize: 50000000
     };
   }
-}
+};
