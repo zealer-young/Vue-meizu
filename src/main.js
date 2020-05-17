b3e88fbb58f36c18ca921f540221ff84ac2b4a75
zealer-young <nightwish.ethan@gmail.com>
2020-05-17 13:12:09 +0800

    change base url of prod

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import App from './App.vue'
 //接口代理：我们当前接口的域名和前端接口域名是一样的
 //(下面这句话的意思)当我们是接口代理的时候
 if (process.env.NODE_ENV === "'prod'" || process.env.NODE_ENV === "'production'") {
-  axios.defaults.baseURL = "http://prev-mall-pre.springboot.cn/api";
+  axios.defaults.baseURL = "http://mall-pre.springboot.cn/api";
 } else {
   axios.defaults.baseURL = "/api";
 }
